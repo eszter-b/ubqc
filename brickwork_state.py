d3479baa45e91163b7d7ec66e1f6ac76f5fbab15
eszter-b <buriseszter@gmail.com>
2024-05-13 15:25:09 +0200

    added fixed graph for testing

--- brickwork_state.py
+++ brickwork_state.py
@@ -14,6 +14,7 @@ def brickwork_graph(n, m):
             for i in range(n):
                 matrix[i][j]=cntr
                 cntr += 1
+        #print(matrix)
 
         for i in range(n):
             for j in range(m):
@@ -36,6 +37,20 @@ def brickwork_graph(n, m):
         print("dimension of m wrong")
         return -1
 
+def fixed_graph_2_bit():
+    G = nx.DiGraph()
+    G.add_node(0, pos=(0, 0))
+    G.add_node(1, pos=(0, 1))
+    G.add_node(2, pos=(1, 1))
+    G.add_node(3, pos=(1, 0))
+    G.add_node(4, pos=(2, 0))
+    G.add_node(5, pos=(2, 1))
+    G.add_node(6, pos=(3, 1))
+    G.add_node(7, pos=(3, 0))
+
+    G.add_edges_from([(0, 1), (0, 3), (1, 2), (2, 5), (3, 4), (4, 7), (5 ,6), (6, 7)])
+
+    return G
 
 def test_edges(G: nx.Graph, m):
     for edge, node in zip(G.edges, G.nodes):
@@ -51,9 +66,35 @@ if __name__ == "__main__":
     m = 1*8+5
 
     G = nx.Graph()
-    G = brickwork_graph(n, m)
+    G = fixed_graph_2_bit()
+    #G = brickwork_graph(n, m)
     #test_edges(G, m)
-    pos = nx.get_node_attributes(G, 'pos')
-    nx.draw(G, pos, with_labels = True)
+    #pos = nx.get_node_attributes(G, 'pos')
 
-    plt.savefig("graph.png")
+    wire_0 = []
+    wire_1 = []
+    for i in range(8):
+        wire = G.nodes[i]['pos']
+        x, y = wire
+        E = G.edges(i)
+        for edge in E:
+            k, l = edge
+            int(k)
+            int(l)
+            if y==0:
+                wire_0.append(k)
+                wire_1.append(0)
+            else:
+                wire_1.append(k)
+                wire_0.append(0)
+
+
+    print("wire0: ", wire_0)
+    print("wire1: ", wire_1)
+    print(G.edges)
+
+        
+    #nx.draw(G, pos, with_labels = True)
+
+    #plt.savefig("graph.png")
+    #plt.savefig("graph_2_bit_Grover")
