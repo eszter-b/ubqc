4f5109011b7399b56eee99c0ac28abeeafac35fa
eszter-b <buriseszter@gmail.com>
2024-05-19 11:51:27 +0200

    fixed graph

--- server_program.py
+++ server_program.py
@@ -50,21 +50,28 @@ class ServerProgram(Program):
 
         # Receive EPR Pairs
         for i in range(num_qubits):
-            brickwork.append(recv_remote_state_preparation(epr_socket))
+            if i == 0:
+                brickwork[0] = recv_remote_state_preparation(epr_socket)
+            else:
+                brickwork.append(recv_remote_state_preparation(epr_socket))
         
         # reverse to match the qubit_id to place in list
+        yield from conn.flush()
         brickwork.reverse()
-        
-        # Prepare brickwork state from received qubits
-        brickwork[0].cphase(brickwork[1])
-        brickwork[1].cphase(brickwork[3])
-        brickwork[0].cphase(brickwork[2])
-        brickwork[2].cphase(brickwork[4])
-        brickwork[3].cphase(brickwork[5])
-        brickwork[5].cphase(brickwork[7])
-        brickwork[4].cphase(brickwork[6])
-        brickwork[6].cphase(brickwork[7])
+        yield from conn.flush()
 
+        # Prepare brickwork state from received qubits
+        #"""
+        brickwork[0].cphase(brickwork[1])
+        brickwork[0].cphase(brickwork[3])
+        brickwork[1].cphase(brickwork[2])
+        brickwork[2].cphase(brickwork[5])
+        brickwork[3].cphase(brickwork[4])
+        brickwork[4].cphase(brickwork[7])
+        brickwork[5].cphase(brickwork[6])
+        brickwork[6].cphase(brickwork[7])
+        #"""
+    
         """
         for edge in G.edges:
             k, l = edge
@@ -80,31 +87,31 @@ class ServerProgram(Program):
             msg = yield from csocket.recv()
             assert msg == "delta sent"
             csocket.send("delta recieved")
-            
+
             brickwork[i].rot_Z(angle=delta)
             brickwork[i].H()
 
-            #if i >= num_qubits-2:
-            yield from conn.flush()
-            msg = yield from csocket.recv()
-            assert msg == "ping"
+            if i >= num_qubits-2:
+                yield from conn.flush()
+                msg = yield from csocket.recv()
+                assert msg == "ping"
 
-            epr_server = get_qubit_state(brickwork[i], "server")
-            fidelity.append(float(
-                dm_fidelity(epr_server, ketutil.reduced_dm(ketstates.b00, [0]))
-            ))
-            csocket.send("pong")
+                epr_server = get_qubit_state(brickwork[i], "server")
+                fidelity.append(float(
+                    dm_fidelity(epr_server, ketutil.reduced_dm(ketstates.b00, [0]))
+                ))
+                csocket.send("pong")
 
-            m = brickwork[i].measure(inplace=True)
+            m = brickwork[i].measure()
             yield from conn.flush()
             csocket.send("qubit measured")
             measurement.append(int(m))
 
             csocket.send_int(measurement[i])
 
-        print(f"fidelity: {fidelity}") 
-        print(f"number of measured qubits: {len(measurement)}")
-        print(f"measurement results: {measurement}")
+        #print(f"fidelity: {fidelity}") 
+        #print(f"number of measured qubits: {len(measurement)}")
+        #print("m: ", measurement)
 
-        return measurement
+        return {"measurement": measurement, "m7": measurement[6], "m8": measurement[7], "f7": fidelity[0], "f8": fidelity[1]}
             
